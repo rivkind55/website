c30d044d267568f1509adaef312f10c33bbae5d9
daniel rivkin <rivkind@wit.edu>
2024-11-13 09:01:02 -0500

    Init Commit of website

---
+++ .idea/script.js
@@ -0,0 +1,15 @@
+
+    let a = 5;
+    let b = 10;
+    let input = 0;
+
+    document.getElementById("firstButton").addEventListener("click", function() {
+        alert("You clicked the button, bro!");
+    });
+
+
+
+
+
+
+
